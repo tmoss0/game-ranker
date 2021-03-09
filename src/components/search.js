965821d0afe79bc907cafe485fb5b9a1c752f891
tmoss0 <tmoss726@hotmail.com>
2021-03-08 19:16:29 -0500

    Update: project begin, Tailwind added

---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+    return(
+        <div className="flex justify-center">
+            <h1 className="text-6xl">Hello World</h1>
+        </div>
+    ) 
+}
+
+export default Search;
